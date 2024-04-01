7e5b2a46f138988238f27b0b20d62a3fe92445fc
Vlada1515 <vladavalianova5@gmail.com>
2024-04-01 12:32:29 +0200

    add

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_KEY = '43019622-51d65222f4845ee303d6eaf7d';
+const baseURL = 'https://pixabay.com/api/';
+
+export async function fetchImages(searchQuery) {
+ try {
+    const response = await axios.get(baseURL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: perPage,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
